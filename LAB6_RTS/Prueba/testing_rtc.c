486bc38ea13e1eab4bcde7442027e15bac129eb9
CerealKilleer <yonathanlopezmejia@gmail.com>
2025-11-16 18:16:55 -0500

    Solucionados unos bugs

--- LAB6_RTS/Prueba/testing_rtc.c
+++ LAB6_RTS/Prueba/testing_rtc.c
@@ -10,7 +10,7 @@ int main() {
 
     /*<! Los archivos de texto o binarios se abren con la llamada al sistema Open, en este caso, el RTC se abre en modo de solo lectura (porque lo que se va a hacer es leer).
      * Cuando se abre un archivo con Open se crea un File Descriptor (fd), esto es simplemente un número entero que identifica al archivo abierto. Si todo sale bien el valor es no
-     * negativo, aquí se trata de abrir el archivo que representa el rtc y se valida si se pudo abrir correctamente */*
+     * negativo, aquí se trata de abrir el archivo que representa el rtc y se valida si se pudo abrir correctamente */
     int fd = open(dev, O_RDONLY);
     if (fd < 0) {
         perror("Error abriendo el RTC");
@@ -18,11 +18,11 @@ int main() {
     }
 
     /*<! Esta estructura permite almacenar los campos que retorna una medición del rtc: segundos, minutos, horas, dia del mes, mes, año. El dia de la semana, del año y la bandera que
-     *indica si se debe adelantar o no una hora deacuerdo al tiempo de verano o invierno, no se utilizan*/*
+     *indica si se debe adelantar o no una hora deacuerdo al tiempo de verano o invierno, no se utilizan*/
     struct rtc_time rtc;
 
     /*<! Algunos archivos abiertos como el del RTC pueden recibir comandos para realizar ciertas acciones por medio de parámetros especiales, ioctl permite realizar eso. En este caso hacer una lectura de la marca de
-     tiempo y almacenarla en la estructura anterior. Si retorna un valor negativo esto indica que ocurrió un error.*/*
+     tiempo y almacenarla en la estructura anterior. Si retorna un valor negativo esto indica que ocurrió un error.*/
     if (ioctl(fd, RTC_RD_TIME, &rtc) < 0) {
         perror("Error leyendo el RTC (ioctl)");
         close(fd); //Claramente hay que cerrar el archivo abierto, porque consume memoria. Es una buena práctica aunque luego del return igual se libera.
